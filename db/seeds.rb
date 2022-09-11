42aecd2ae40eda70dfefddcc9705aba9e4212477
ClaudiaRuizUX <claudiaruizux@gmail.com>
2022-09-11 14:52:09 +1000

    add many through relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create({title: "hello", content: "world"})
+
+Post.create({title: "Hi", content: "Nice to meet you"})
+
+User.create({username: "Joe", email: "joe@info.com"})
+
+User.create({username: "Jane", email: "jane@info.com"})
+
+Comment.create({content: "that is good", post_id: 1, user_id: 1})
+Comment.create({content: "not too bad", post_id: 1, user_id: 2})
+Comment.create({content: "its bad", post_id: 2, user_id: 2})
+
+
